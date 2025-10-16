e4de4683384038f863efea7c2ddebd08abdc5929
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-16 19:24:23 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,78 @@
+﻿//int[,] matrix = new int[3, 4];
+
+//int[,] matrix =
+//{
+//    new Array[] {1, 2, 3 },
+//    { 4, 5, 6 }
+//};
+
+//matrix.ForEach(row => Console.WriteLine(string.Join(" ", row)));
+
+//////////////////////////////////
+
+
+
+//int[] input = Console.ReadLine()
+//    .Split().
+//    Select(int.Parse).
+//    ToArray();
+
+//int rows = input[0];
+//int cols = input[1];
+
+///////////////////////////////////////////////
+//int[,] matrix = ReadMatrix();
+
+//static int[,] ReadMatrix()
+//{
+//    //int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//    Console.WriteLine("Rows: ");
+//    int rows = int.Parse(Console.ReadLine());
+
+//    Console.WriteLine("Cols: ");
+//    int cols = int.Parse(Console.ReadLine());
+
+//    int[,] matrix = new int[rows, cols];
+
+//    for (int i = 0; i < rows; i++)
+//    {
+//        int[] matrixContent = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//        for (int j = 0; j < cols; j++)
+//        {
+//            matrix[i, j] = matrixContent[j];
+//        }
+//    }
+
+//    return matrix;
+//}
+
+////////////////////////////////////////////////
+
+
+///
+
+int rows = 3;
+int cols = 3;
+
+int[,] matrix = ReadMatrix(rows, cols);
+
+static int[,] ReadMatrix(int rows, int cols)
+{
+    int[,] resultMatrix = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        int[] input = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int j = 0; j < cols; j++)
+        {
+            resultMatrix[i, j] = input[j];
+        }
+    }
+
+    return resultMatrix;
+}
---
+++ SumMatrixElements/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] inputData = Console.ReadLine()
+    .Split(", ")
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = inputData[0];
+int cols = inputData[1];
+
+int[,] matrix = ReadMatrix(rows, cols);
+
+Console.WriteLine(rows);
+Console.WriteLine(cols);
+
+PrintSumOfMatrixElements(matrix);
+
+
+static int[,] ReadMatrix(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        int[] currentRow = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = currentRow[j];
+        }
+    }
+
+    return matrix;
+}
+
+
+static void PrintSumOfMatrixElements(int[,] inputMatrix)
+{
+    int sum = 0;
+
+    for (int row = 0; row < inputMatrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < inputMatrix.GetLength(1); col++)
+        {
+            sum += inputMatrix[row, col];
+        }
+    }
+
+    Console.WriteLine(sum);
+}
