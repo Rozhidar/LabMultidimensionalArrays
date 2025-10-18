c6f3f429fb0cc6777657eec8ae85a12e6bb37894
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-18 16:47:34 +0200

    add project

---
+++ SymbolInMatrix/Program.cs
@@ -0,0 +1,51 @@
+﻿int matrixParameters = int.Parse(Console.ReadLine());
+
+int rows = matrixParameters;
+int cols = matrixParameters;
+
+char[,] matrix = ReadMatrix(rows, cols);
+
+char targetSymbol = char.Parse(Console.ReadLine());
+
+bool isFound = false;
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        char currentSymbol = matrix[i, j];
+
+        if (currentSymbol == targetSymbol)
+        {
+            isFound = true;
+            Console.WriteLine($"({i}, {j})");
+            return;
+        }
+    }
+}
+
+if (!isFound)
+{
+    Console.WriteLine($"{targetSymbol} does not occur in the matrix");
+}
+
+
+static char[,] ReadMatrix(int rows, int cols)
+{
+    char[,] matrix = new char[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        char[] inputCharacters = Console.ReadLine().ToCharArray();
+
+        if (inputCharacters.Length == cols)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = inputCharacters[j];
+            }
+        }
+    }
+
+    return matrix;
+}
